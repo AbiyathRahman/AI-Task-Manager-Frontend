df3552b3f43d3a2785238068cba0a044a3ec68ba
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2025-06-27 15:45:29 -0600

    Created a skeleton for task craetion in the dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,12 +3,37 @@ import { useEffect, useState } from "react";
 
 export default function Dashboard(){
     const [tasks, setTasks] = useState([]);
+    const [form, setForm] = useState({taskName: '', taskDescription: '', taskPriority: '', taskStatus: '', taskDueDate: ''});
 
     const loadTask = async () => {
         const res = await axios.get('/task/my-tasks');
         setTasks(res.data);
     };
     useEffect(( ) => {loadTask();}, []);
+
+    const handleChange = e => setForm({...form, [e.target.name]:e.target.value});
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        try {
+            await axios.post('/task/create', form, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            alert('Task created successfully');
+            setForm({taskName: '', taskDescription: '', taskPriority: '', taskStatus: '', taskDueDate: ''}); // Reset form
+        loadTask();
+
+
+
+            
+        } catch (error) {
+            console.log(error);
+            alert('error creating task');
+            
+        }
+    }
 
     return(
         <div>
@@ -20,6 +45,15 @@ export default function Dashboard(){
                     </li>
                 ))}
             </ul>
+            <form onSubmit={handleSubmit}>
+            <h2>Create Tasks</h2>
+            <input name="taskName" onChange={handleChange} placeholder="task name"></input>
+            <input name="taskDescription" onChange={handleChange} placeholder="task description"></input>
+            <input name="taskStatus" onChange={handleChange} placeholder="task status"></input>
+            <input name="taskPriority" onChange={handleChange} placeholder="task priority"></input>
+            <input name="taskDueDate" onChange={handleChange} placeholder="task due date"></input>
+            <button type="submit">Create</button>
+            </form>
 
         </div>
 
