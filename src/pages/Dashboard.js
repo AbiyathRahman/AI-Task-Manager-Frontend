a5987a51b4457cb2d808be30ea2f0f53327c5b73
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2025-06-29 21:56:50 -0600

    Created the ability to delete tasks, also created dropdown for priority and status

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -60,6 +60,23 @@ export default function Dashboard(){
         setIsUpdate(true);
         setSelectedTaskId(task.id);
     }
+    const handleDelete = async (task) => {
+        try {
+            await axios.delete(`/task/${task.id}`);
+            alert("Deleted successfully")
+
+            
+        } catch (error) {
+            console.log(error)
+            alert(`error deleting task ${task.id}`);
+            
+        }
+        loadTask();
+        
+
+
+
+    }
 
     const handleCancel = () => {
         // Reset form and states
@@ -76,6 +93,7 @@ export default function Dashboard(){
                     <li key={t.id}>
                         <b>{t.taskName}</b> - {t.taskDescription}
                         <button onClick={() => handleUpdate(t)}>Update</button>
+                        <button onClick={() => handleDelete(t)}>Delete</button>
                     </li>
                 ))}
             </ul>
@@ -83,11 +101,25 @@ export default function Dashboard(){
             <h2>{isUpdate ? 'Update Task' : 'Create Task'}</h2>
             <input name="taskName" value={form.taskName} onChange={handleChange} placeholder="task name"></input>
             <input name="taskDescription" value={form.taskDescription} onChange={handleChange} placeholder="task description"></input>
-            <input name="taskStatus" value={form.taskStatus} onChange={handleChange} placeholder="task status"></input>
-            <input name="taskPriority" value={form.taskPriority} onChange={handleChange} placeholder="task priority"></input>
+            
+            <select name="taskStatus" value={form.taskStatus} onChange={handleChange}>
+                <option value="">Select Status</option>
+                <option value="INPROGRESS">In Progress</option>
+                <option value="OPEN">OPEN</option>
+                <option value="CLOSED">CLOSED</option>
+            </select>
+            
+            <select name="taskPriority" value={form.taskPriority} onChange={handleChange}>
+                <option value="">Select Priority</option>
+                <option value="LOW">Low</option>
+                <option value="MEDIUM">Medium</option>
+                <option value="HIGH">High</option>
+            </select>
+            
             <input name="taskDueDate" value={form.taskDueDate} onChange={handleChange} placeholder="task due date"></input>
             <button type="submit">{isUpdate ? 'Update' : 'Create'}</button>
             {isUpdate && <button type="button" onClick={handleCancel}>Cancel</button>}
+            
             </form>
 
         </div>
